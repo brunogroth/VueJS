ef0143c53d488105ac5e349b333d0a79874974e7
Bruno Groth <brunom.groth@gmail.com>
2023-07-06 07:56:55 -0300

    Feat: First Vue App: Goals List. Concepts.

--- 01_Concepts/ProjectGoals/app.js
+++ 01_Concepts/ProjectGoals/app.js
@@ -1,14 +1,29 @@
-var btn = document.querySelector('button');
-var input = document.querySelector('input');
-var list = document.querySelector('ul');
+Vue.createApp({
+  data() { // this is the same as data: function() {}
+    return {
+      goals: [],
+      enteredValue: ''
+    };
+  },
+  methods: { 
+    addGoal() {
+      this.goals.push(this.enteredValue);
+      this.enteredValue = '';
+    }
+  },
+}).mount('#app');
 
-function addItem(){
-    var text = input.value;
-    var listItem = document.createElement('li');
-    listItem.textContent = text;  
-    list.appendChild(listItem);
-    input.value = '';
+// var btn = document.querySelector('button');
+// var input = document.querySelector('input');
+// var list = document.querySelector('ul');
 
-}
+// function addItem(){
+//     var text = input.value;
+//     var listItem = document.createElement('li');
+//     listItem.textContent = text;
+//     list.appendChild(listItem);
+//     input.value = '';
 
-btn.addEventListener('click', addItem);
+// }
+
+// btn.addEventListener('click', addItem);
