364432f01cc0a7ec41fd842a72c995510c8c367a
Bruno Groth <brunom.groth@gmail.com>
2023-07-10 20:45:11 -0300

    Feat: First dinamic properties, first methods, learning data binding in Vue

--- 01_Concepts/ProjectGoals/app.js
+++ 01_Concepts/ProjectGoals/app.js
@@ -1,9 +1,21 @@
+
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: 'Finish the course and learn Vue.'
+      courseGoal: '',
+      vueLink: 'https://vuejs.org'
     };
-  }
-});
+  },
+  methods: {
+    outputGoal() { // same as outputGoal: function() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5){
+        this.courseGoal = 'Learn Vue!'
+      } else {
+        this.courseGoal = 'Master Vue!'
+      }
+    },
 
+  },
+});
 app.mount('#user-goal');
