5933d8987401707978c911bf7fb16f3c84a73994
Bruno Groth <brunom.groth@gmail.com>
2023-07-10 21:42:27 -0300

    Events - Native Event Object & Event Modifiers

--- 01_Concepts/Events/app.js
+++ 01_Concepts/Events/app.js
@@ -3,9 +3,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: '',
     };
   },
   methods: {
+    setName(event) {
+      this.name = event.target.value; 
+    },
+    confirmName(){
+      this.confirmedName = this.name;
+    },
+    increment() {
+      this.counter = this.counter + 1;
+    },
+    decrement(){
+      this.counter = this.counter - 1;
+    }
     },
 
 
