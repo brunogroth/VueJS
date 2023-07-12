64e7174119c9534e917283ff8bcf524e498eb286
Bruno Groth <brunom.groth@gmail.com>
2023-07-11 22:51:13 -0300

    Learning: working with Watchers

--- 01_Concepts/Events/app.js
+++ 01_Concepts/Events/app.js
@@ -4,16 +4,18 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
+      lastName: '',
+      // fullName: '',
       confirmedName: '',
     };
   },
   computed: {
     fullName(){
       console.log('run')
-      if(this.name === ''){
+      if(this.name === '' || this.lastName){
         return '';
       }
-      return this.name + ' Groth'
+      return this.name + ' ' + this.lastName
     }
   },
   methods: {
@@ -30,7 +32,26 @@ const app = Vue.createApp({
       this.counter = this.counter - 1;
     },
     },
-
-
+    watch: {
+      counter(value) {
+        if(value > 5) {
+          this.counter = 0;
+        }
+      }
+      // name(value){
+      //   if(value === ''){
+      //     this.fullName = '';
+      //   } else {
+      //     this.fullName = value + ' ' + this.lastName;
+      //   }
+      // },
+      // lastName(value){
+      //   if (value === ''){
+      //     this.fullName = '';
+      //   } else {
+      //     this.fullname = this.name + ' ' + value;
+      //   }
+      // }
+    }
 });
 app.mount('#user-goal');
