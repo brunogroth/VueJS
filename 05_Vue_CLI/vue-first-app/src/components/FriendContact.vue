5e4a51ced93fe6cbdd7d6bfc8d1c141f0d2cfbde
Bruno Miguel Groth <brunom.groth@gmail.com>
2023-08-19 17:33:05 -0300

    Feat: First vue Components | Add styles in .vue

---
+++ 05_Vue_CLI/vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>Phone: {{ friend.phone }}</li>
+      <li>Email: {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: 1,
+        name: "John Galt",
+        phone: "1234-5678",
+        email: "johngalt@localhost.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  },
+};
+</script>
