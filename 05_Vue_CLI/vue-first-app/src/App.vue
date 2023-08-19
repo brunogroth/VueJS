677aaf098dfa30d18a74b938e4cf715d317aad99
Bruno Miguel Groth <brunom.groth@gmail.com>
2023-08-19 16:30:35 -0300

    First Vue App

---
+++ 05_Vue_CLI/vue-first-app/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul v-for="(friend, index) in friendList" :key="index">
+      <li>{{ friend.name }}</li>
+      <li>Phone: {{ friend.phone }}</li>
+      <li>Email: {{ friend.email }}</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friendList: [
+        {
+          id: 1,
+          name: "John Galt",
+          phone: "1234-5678",
+          email: "john@galt.com",
+        },
+        {
+          id: 2,
+          name: "Johanna",
+          phone: "1234-5678",
+          email: "Johanna@localhost.com",
+        }
+      ],
+    };
+  },
+};
+</script>
