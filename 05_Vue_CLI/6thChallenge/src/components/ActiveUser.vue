87d160703a635d576615e980525925372b472848
Bruno <brunom.groth@gmail.com>
2023-08-27 12:40:12 -0300

    Feat: Challenge 6 - Props and Custom Events with Components

---
+++ 05_Vue_CLI/6thChallenge/src/components/ActiveUser.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <form>
+      <label for="name">Your name:</label>
+      <input
+        type="text"
+        id="name"
+        v-model="user.name"
+        @change.prevent="updateUserData"
+      />
+      <div>
+        <label for="age">Your age:</label>
+        <input
+          type="number"
+          id="age"
+          v-model="user.age"
+          @change.prevent="updateUserData"
+        />
+      </div>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["user-change"],
+  data() {
+    return {
+      user: {
+        name: "",
+        age: "",
+      },
+    };
+  },
+  methods: {
+    updateUserData() {
+      this.$emit("user-change", this.user);
+    },
+  },
+};
+</script>
---
+++ 05_Vue_CLI/6thChallenge/src/components/UserData.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <div class="userInformation">
+      <h2>{{ name }}</h2>
+      <h3>{{ age }} Years</h3>
+    </div>
+  </section>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+<style>
+.userInformation {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+</style>
+
