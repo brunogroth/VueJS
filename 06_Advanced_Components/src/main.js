fa25e6423e16a1ab3e1fa21bc2cdb717ca80f761
Bruno <brunom.groth@gmail.com>
2023-09-02 21:00:34 -0300

    Feat: about Scoped Styles, Slots and Named Slots

--- 06_Advanced_Components/src/main.js
+++ 06_Advanced_Components/src/main.js
@@ -5,9 +5,10 @@ import TheHeader from "./components/TheHeader.vue";
 import BaseBadge from "./components/BaseBadge.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import BaseCard from "./components/BaseCard.vue";
 
 const app = createApp(App);
 
 app.component("base-badge", BaseBadge);
-
+app.component("base-card", BaseCard);
 app.mount("#app");
