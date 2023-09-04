9520dabfe96ea55c52f5106feee64155bfb74065
Bruno <brunom.groth@gmail.com>
2023-09-03 22:58:55 -0300

    Feat: Manage Goal, Goal validation'

--- 06_Advanced_Components/src/components/ManageGoals.vue
+++ 06_Advanced_Components/src/components/ManageGoals.vue
@@ -1,3 +1,39 @@
 <template>
-  <h2>Manage Goals</h2>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>A goal cannot be empty.</p>
+      <button @click="closeDialog">Okay</button>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    closeDialog() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
