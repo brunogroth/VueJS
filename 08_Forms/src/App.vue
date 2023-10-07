9088a5d78da4c177e9b46e618cac668f3a2f9a92
Bruno <brunom.groth@gmail.com>
2023-10-07 00:19:49 -0300

    Feat: implementing Forms

---
+++ 08_Forms/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <TheForm />
+</template>
+<script>
+import TheForm from "./components/TheForm.vue";
+
+export default {
+  components: {
+    TheForm,
+  },
+};
+</script>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+  background-color: #ffffff;
+}
+</style>
