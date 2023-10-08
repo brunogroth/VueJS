31f90f6bea4328c5ba45f4ecb98d02a3f6222f80
Bruno <brunom.groth@gmail.com>
2023-10-08 14:21:42 -0300

    Setup Learning Experience Project - Feat: Submit and List Experiences

---
+++ 09_HTTP_Requests/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <learning-experience @survey-submit="storeSurvey" />
+    <user-experiences :results="savedSurveyResults" />
+  </div>
+</template>
+
+<script>
+import LearningExperience from "./components/survey/LearningExperience.vue";
+import UserExperiences from "./components/survey/UserExperiences.vue";
+
+export default {
+  components: {
+    LearningExperience,
+    UserExperiences,
+  },
+
+  data() {
+    return {
+      savedSurveyResults: [],
+    };
+  },
+
+  methods: {
+    storeSurvey(surveyData) {
+      const surveyResult = {
+        name: surveyData.userName,
+        rating: surveyData.rating,
+        id: new Date().toISOString(),
+      };
+      this.savedSurveyResults.push(surveyResult);
+      console.log(surveyResult);
+      console.log(this.savedSurveyResults);
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
---
+++ 09_HTTP_Requests/src/components/survey/UserExperiences.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button>Update Submitted Experiences</base-button>
+      </div>
+      <ul>
+        <survey-result
+          v-for="result in results"
+          :survey="result"
+          :name="result.name"
+          :rating="result.rating"
+          :key="result.id"
+        >
+        </survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from "./SurveyResult.vue";
+// import SurveyResult from "./components/survey/SurveyResult.vue";
+export default {
+  props: ["results"],
+
+  components: {
+    SurveyResult,
+    // SurveyResult,
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
