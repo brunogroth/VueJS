5c0a3a1695d98c9ab438809a81812253a6eaf836
Bruno <brunom.groth@gmail.com>
2023-10-15 12:13:18 -0300

    Feat: Setup project & Install VueX

---
+++ 12_VueX_Managing_State/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseContainer title="Vuex"></BaseContainer>
+</template>
+
+<script>
+import BaseContainer from "./components/BaseContainer.vue";
+
+export default {
+  components: {
+    BaseContainer,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
