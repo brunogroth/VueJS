38eec0a770a3da37f79bbeb6b206857c31e314b0
Bruno <brunom.groth@gmail.com>
2023-10-16 11:47:02 -0300

    Feat: VueX Mapper Helpers

--- 12_VueX_Managing_State/src/components/ChangeCounter.vue
+++ 12_VueX_Managing_State/src/components/ChangeCounter.vue
@@ -1,15 +1,33 @@
 <template>
-  <button @click="addOne">Add 1</button>
+  <div>
+    <button @click="addOne">Add 1 using VueX in Vue Method</button> |
+    <button @click="addMultiple({ value: 10 })">
+      Add 10 using VueX in Vue Method
+    </button>
+    |
+    <button @click="increase({ value: 10 })">
+      Add 10 using VueX Mapper Helper
+    </button>
+  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
   methods: {
     addOne() {
-      // this.$store.state.counter++; // not ideal practice
+      // this.$store.state.counter++; // NOT RECOMMENDED practice
       // this.$store.commit("increment"); // ok practice - only synchronous code - execute the mutation (method "increment")
       this.$store.dispatch("increment"); // best practice - execute the actions that runs the mutation (method "increment")
     },
+    addMultiple(payload) {
+      // this.$store.state.counter++; // NOT RECOMMENDED practice
+      // this.$store.commit("increment"); // ok practice - only synchronous code - execute the mutation (method "increment")
+      this.$store.dispatch("increase", payload); // best practice - execute the actions that runs the mutation (method "increment")
+    },
+
+    // OR, using VueX Mapper Helpers:
+    ...mapActions(["increase"]),
   },
 };
 </script>
